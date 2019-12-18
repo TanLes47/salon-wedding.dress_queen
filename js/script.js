967ecebfa8a628d59cf2ec5c129ee000151eb196
Tetyana <tetyana.lesivtsiv@gmail.com>
2019-12-18 15:11:55 +0200

    Add code and changes

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ function login(e) {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            // 'Content-Type': 'application/x-www-form-urlencoded',
         },
         body: JSON.stringify({
             name: document.querySelector('.login-form input[name=name]').value,
@@ -18,7 +17,31 @@ function login(e) {
         .then(_ => document.querySelector('.login-form').reset());
 }
 
-const list = document.querySelector('.list_item');
+
+const list = document.querySelector('ul');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
+
+button.addEventListener('click', btnClick );
+
+function btnClick(e) {
+    e.preventDefault();
+    let item = input.value;
+    input.value = '';
+
+    const listItem = document.createElement('li');
+    const listText = document.createElement('span');
+    const listBtn = document.createElement('button');
+
+    listItem.appendChild(listText);
+    listText.textContent = item;
+    listItem.appendChild(listBtn);
+    listBtn.textContent = 'Delete';
+    list.appendChild(listItem);
+
+    listBtn.btnClick = function(e) {
+      list.removeChild(listItem);
+    }
+    input.focus();
+  }
 
